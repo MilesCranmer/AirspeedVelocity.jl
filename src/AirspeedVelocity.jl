646b793fe3c95929b8973b88d9723a95e4d51a3d
MilesCranmer <miles.cranmer@gmail.com>
2023-04-03 01:38:01 -0400

    Clean up API

--- src/AirspeedVelocity.jl
+++ src/AirspeedVelocity.jl
@@ -81,6 +81,77 @@ function _benchmark(
     return nothing
 end
 
+"""
+    benchmark(package_name::String, rev::Union{String,Vector{String}}; output_dir::String=".", benchmark_script::Union{String,Nothing}=nothing, tune::Bool=false, exeflags::Cmd=``)
+
+Run benchmarks for a given Julia package.
+
+This function runs the benchmarks specified in the `benchmark_script` for the package defined by the `package_spec`. If `benchmark_script` is not provided, the function will use the default benchmark script located at `{PACKAGE_SRC_DIR}/benchmark/benchmarks.jl`.
+
+The benchmarks are run using the `SUITE` variable defined in the benchmark script, which should be of type BenchmarkTools.BenchmarkGroup. The benchmarks can be run with or without tuning depending on the value of the `tune` argument.
+
+The results of the benchmarks are saved to a JSON file named `results_packagename@rev.json` in the specified `output_dir`.
+
+# Arguments
+- `package_name::String`: The name of the package for which to run the benchmarks.
+- `rev::Union{String,Vector{String}}`: The revision of the package for which to run the benchmarks. You can also pass a vector of revisions to run benchmarks for multiple versions of a package.
+- `output_dir::String="."`: The directory where the benchmark results JSON file will be saved (default: current directory).
+- `benchmark_script::Union{String,Nothing}=nothing`: The path to the benchmark script file. If not provided, the default script at `{PACKAGE}/benchmark/benchmarks.jl` will be used.
+- `tune::Bool=false`: Whether to run benchmarks with tuning (default: false).
+- `exeflags::Cmd=```: Additional execution flags for running the benchmark script (default: empty).
+"""
+function benchmark(
+    package_name::String,
+    revs::Vector{String};
+    output_dir::String=".",
+    benchmark_script::Union{String,Nothing}=nothing,
+    tune::Bool=false,
+    exeflags::Cmd=``,
+)
+    return benchmark(
+        [PackageSpec(; name=package_name, rev=rev) for rev in revs];
+        output_dir=output_dir,
+        benchmark_script=benchmark_script,
+        tune=tune,
+        exeflags=exeflags,
+    )
+end
+function benchmark(
+    package_name::String,
+    rev::String;
+    output_dir::String=".",
+    benchmark_script::Union{String,Nothing}=nothing,
+    tune::Bool=false,
+    exeflags::Cmd=``,
+)
+    return benchmark(
+        package_name,
+        [rev];
+        output_dir=output_dir,
+        benchmark_script=benchmark_script,
+        tune=tune,
+        exeflags=exeflags,
+    )
+end
+
+"""
+    benchmark(package::Union{PackageSpec,Vector{PackageSpec}}; output_dir::String=".", benchmark_script::Union{String,Nothing}=nothing, tune::Bool=false, exeflags::Cmd=``)
+
+Run benchmarks for a given Julia package.
+
+This function runs the benchmarks specified in the `benchmark_script` for the package defined by the `package_spec`. If `benchmark_script` is not provided, the function will use the default benchmark script located at `{PACKAGE_SRC_DIR}/benchmark/benchmarks.jl`.
+
+The benchmarks are run using the `SUITE` variable defined in the benchmark script, which should be of type BenchmarkTools.BenchmarkGroup. The benchmarks can be run with or without tuning depending on the value of the `tune` argument.
+
+The results of the benchmarks are saved to a JSON file named `results_packagename@rev.json` in the specified `output_dir`.
+
+# Arguments
+- `package::Union{PackageSpec,Vector{PackageSpec}}`: The package specification containing information about the package for which to run the benchmarks. You can also pass a vector of package specifications to run benchmarks for multiple versions of a package.
+- `output_dir::String="."`: The directory where the benchmark results JSON file will be saved (default: current directory).
+- `benchmark_script::Union{String,Nothing}=nothing`: The path to the benchmark script file. If not provided, the default script at `{PACKAGE}/benchmark/benchmarks.jl` will be used.
+- `tune::Bool=false`: Whether to run benchmarks with tuning (default: false).
+- `exeflags::Cmd=```: Additional execution flags for running the benchmark script (default: empty).
+"""
 function benchmark(
     package_spec::PackageSpec;
     output_dir::String=".",
@@ -106,40 +177,6 @@ function benchmark(
     for spec in package_specs
         benchmark(spec; output_dir, benchmark_script, tune, exeflags)
     end
-end
-
-function benchmark(
-    package_name::String,
-    revs::Vector{String};
-    output_dir::String=".",
-    benchmark_script::Union{String,Nothing}=nothing,
-    tune::Bool=false,
-    exeflags::Cmd=``,
-)
-    return _benchmark(
-        [PackageSpec(; name=package_name, rev=rev) for rev in revs];
-        output_dir=output_dir,
-        benchmark_script=benchmark_script,
-        tune=tune,
-        exeflags=exeflags,
-    )
-end
-function benchmark(
-    package_name::String,
-    rev::String;
-    output_dir::String=".",
-    benchmark_script::Union{String,Nothing}=nothing,
-    tune::Bool=false,
-    exeflags::Cmd=``,
-)
-    return benchmark(
-        package_name,
-        [rev];
-        output_dir=output_dir,
-        benchmark_script=benchmark_script,
-        tune=tune,
-        exeflags=exeflags,
-    )
 end
 
 end
