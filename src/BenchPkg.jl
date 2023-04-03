1f472d3a945d0f6e2907421aa2167646b565127d
MilesCranmer <miles.cranmer@gmail.com>
2023-04-03 05:03:05 -0400

    Make rev a keyword argument

--- src/BenchPkg.jl
+++ src/BenchPkg.jl
@@ -4,15 +4,17 @@ using ..Utils: benchmark
 using Comonicon
 
 """
+    benchpkg package_name [-r --rev <arg>] [-o, --output_dir <arg>] [-s, --script <arg>] [-e, --exeflags <arg>] [-a, --add <arg>] [-t, --tune]
+
 Benchmark a package over a set of revisions.
 
 # Arguments
 
 - `package_name`: Name of the package.
-- `rev`: Revisions to test.
 
 # Options
 
+- `-r --rev <arg>`: Revisions to test (delimit by comma).
 - `-o, --output_dir <arg>`: Where to save the JSON results.
 - `-s, --script <arg>`: The benchmark script. Default: `{PACKAGE_SRC_DIR}/benchmark/benchmarks.jl`.
 - `-e, --exeflags <arg>`: CLI flags for Julia (default: none).
@@ -24,17 +26,21 @@ Benchmark a package over a set of revisions.
 
 """
 @main function benchpkg(
-    package_name::String,
-    rev::String...;
+    package_name::String;
+    rev::String,
     output_dir::String = ".",
     script::String = "",
     exeflags::String = "",
     add::String = "",
     tune::Bool = false,
 )
+    revs = convert(Vector{String}, split(rev, ","))
+    # Filter empty strings:
+    revs = filter(x -> length(x) > 0, revs)
+    @assert length(rev) > 0, "No revisions specified."
     benchmark(
         package_name,
-        [rev...];
+        revs;
         output_dir = output_dir,
         script = (length(script) > 0 ? script : nothing),
         tune = tune,
