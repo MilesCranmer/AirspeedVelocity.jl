d608b486c9c289aa090148edafa602c13f378472
Documenter.jl <documenter@juliadocs.github.io>
2023-04-03 03:58:07 +0000

    build based on 66db1a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AirspeedVelocity","category":"page"},{"location":"#AirspeedVelocity","page":"Home","title":"AirspeedVelocity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AirspeedVelocity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AirspeedVelocity]","category":"page"}]
+}
