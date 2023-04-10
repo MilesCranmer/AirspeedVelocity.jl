ca77be284876249fba50e2bd13f15206c799ea18
MilesCranmer <miles.cranmer@gmail.com>
2023-04-10 13:31:51 -0400

    Create `benchpkgtable` command

--- deps/build.jl
+++ deps/build.jl
@@ -2,3 +2,4 @@ using AirspeedVelocity
 
 AirspeedVelocity.BenchPkg.comonicon_install()
 AirspeedVelocity.BenchPkgPlot.comonicon_install()
+AirspeedVelocity.BenchPkgTable.comonicon_install()
---
+++ src/BenchPkgTable.jl
@@ -0,0 +1,38 @@
+module BenchPkgTable
+
+using ..TableUtils: create_table
+using ..Utils: load_results
+using Comonicon
+
+"""
+    benchpkgtable package_name [-r --rev <arg>] [-i --input-dir <arg>]
+
+Print a table of the benchmarks of a package as created with `benchpkg`.
+
+# Arguments
+
+- `package_name`: Name of the package.
+
+# Options
+
+- `-r, --rev <arg>`: Revisions to test (delimit by comma).
+- `-i, --input-dir <arg>`: Where the JSON results were saved (default: ".").
+
+# Flags
+
+- `--ratio`: Whether to include the ratio (default: false). Only applies when
+    comparing two revisions.
+"""
+@main function benchpkgtable(
+    package_name::String; rev::String, input_dir::String=".", ratio::Bool=false
+)
+    revs = convert(Vector{String}, split(rev, ","))
+    # Filter empty strings:
+    revs = filter(x -> length(x) > 0, revs)
+    @assert length(revs) > 0 "No revisions specified."
+    combined_results = load_results(package_name, revs; input_dir=input_dir)
+
+    return println(create_table(combined_results; add_ratio_col=ratio))
+end
+
+end # AirspeedVelocity.BenchPkgTable
