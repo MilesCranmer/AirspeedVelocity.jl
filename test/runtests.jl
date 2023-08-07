ae90d2d97ffa8ef6e96fce55b99bf52d51a4663c
MilesCranmer <miles.cranmer@gmail.com>
2023-08-07 09:55:05 -0400

    Use Aqua.jl in testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using AirspeedVelocity
+using Aqua
 using Test
 
+@testset "Aqua tests" begin
+    Aqua.test_all(AirspeedVelocity)
+end
 @testset "AirspeedVelocity.jl" begin
     include("test_benchmark.jl")
 end
