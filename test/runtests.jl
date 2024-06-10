e012e336c860426ba9b672496fed9bae6f15f0da
MilesCranmer <miles.cranmer@gmail.com>
2024-06-10 05:07:22 +0100

    min codegen so that code coverage works

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,11 @@ using Test
 using Preferences: set_preferences!
 
 set_preferences!(
-    "AirspeedVelocity", "instability_check" => "error", "instability_check_union_limit" => 2
+    "AirspeedVelocity",
+    "instability_check" => "error",
+    "instability_check_codegen_level" => "min",
+    "instability_check_union_limit" => 2;
+    force=true,
 )
 
 using AirspeedVelocity
