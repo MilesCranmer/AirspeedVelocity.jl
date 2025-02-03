29df1c604929ed00979c42e3c103c7a3de2d0bec
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-02-03 09:30:47 -0600

    Fix more column alignment

--- test/runtests.jl
+++ test/runtests.jl
@@ -214,18 +214,18 @@ end
     truth = """
     |        | v1           | v2         | v1 / v2 |
     |:-------|:------------:|:----------:|:-------:|
-    | bench1 | 1.2 ± 0.2 s  | 12 ± 2 s   | 0.1   |
-    | bench2 | 0.2 ± 0.2 ms | 20 ± 20 μs | 10    |
-    | bench3 |              | 20 ± 20 μs |       |
+    | bench1 | 1.2 ± 0.2 s  | 12 ± 2 s   | 0.1     |
+    | bench2 | 0.2 ± 0.2 ms | 20 ± 20 μs | 10      |
+    | bench3 |              | 20 ± 20 μs |         |
     """
     @test truth ≈ create_table(combined_results)
 
     truth = """
     |        | v1                 | v2                | v1 / v2   |
     |:-------|:------------------:|:-----------------:|:---------:|
-    | bench1 | 1  allocs: 10 B    | 2  allocs: 1 kB   | 0.00977 |
-    | bench2 | 1 M allocs: 0.1 kB | 3  allocs: 10 kB  | 0.01    |
-    | bench3 |                    | 4  allocs: 0.1 MB |         |
+    | bench1 | 1  allocs: 10 B    | 2  allocs: 1 kB   | 0.00977   |
+    | bench2 | 1 M allocs: 0.1 kB | 3  allocs: 10 kB  | 0.01      |
+    | bench3 |                    | 4  allocs: 0.1 MB |           |
     """
     @test truth ≈ create_table(
         combined_results; formatter=AirspeedVelocity.TableUtils.format_memory, key="memory"
