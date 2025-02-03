77d6fc14ac50abc388ce23e125c8c3b3f966c0b7
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-02-03 06:58:08 -0600

    Add whitespace around slash in ratio table header

--- src/TableUtils.jl
+++ src/TableUtils.jl
@@ -65,7 +65,7 @@ Create a markdown table of the results loaded from the `load_results` function.
 If there are two results for a given benchmark, will have an additional column
 for the comparison, assuming the first revision is one to compare against.
 
-The `formatter` keyword argument generates the column value. It defaults to 
+The `formatter` keyword argument generates the column value. It defaults to
 `TableUtils.format_time`, which prints the median time ± the interquantile range.
 `TableUtils.format_memory` is also available to print the number of allocations
 and the allocated memory.
@@ -128,7 +128,7 @@ function create_table(
             end
         end
         push!(data_columns, col)
-        push!(headers, "$(headers[2])/$(headers[3])")
+        push!(headers, "$(headers[2]) / $(headers[3])")
         num_cols += 1
     end
 
